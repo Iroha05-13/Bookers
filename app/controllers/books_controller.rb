8bb9338d14ce3278145d1c705e6bb6bc46ac6c8c
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-03-11 03:17:52 +0000

    [Update]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,30 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
     @books = Book.all
   end
-  
+
   def show
   end
 
   def edit
   end
-  
+
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
   end
 
   def update
   end
-  
+
   def destroy
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
